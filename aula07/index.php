ffaf568dd4b3ae34f4da67ccb87a03b8ee6da88c
Pedro Henrique <pedro.lg.cs@gmail.com>
2017-09-21 12:56:49 -0300

    aula 07 finalizada

---
+++ aula07/index.php
@@ -0,0 +1,23 @@
+<?php
+	include_once 'class/Lutador.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Aula07</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<pre>
+		<?php
+			$l[0] = new Lutador('Pretty Boy', 'França', 31, 1.75, 68.9, 11, 2, 1);
+			$l[1] = new Lutador('Putscript', 'Brasil', 21, 1.68, 57.8, 14, 2, 3);
+			$l[2] = new Lutador('SnapShadow', 'EUA', 35, 1.65, 80.9, 12, 2, 1);
+			$l[3] = new Lutador('Dead Code', 'Austrália', 28, 1.93, 81.6, 13, 0, 2);
+			print_r($l);
+		?>
+	</pre>
+</body>
+</html>
