5de6b386aee36984931f0d3cf7df64572a12b01f
Pedro Henrique <pedro.lg.cs@gmail.com>
2017-09-23 01:42:59 -0300

    aula 08 finalizada

--- aula08/index.php
+++ aula08/index.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 	include_once 'class/Lutador.php';
 	include_once 'class/Luta.php';
 ?>
@@ -12,7 +12,7 @@
 </head>
 <body>
 	<pre>
-		<?php  
+		<?php
 			$l[0] = new Lutador('Pretty Boy', 'França', 31, 1.75, 68.9, 11, 2, 1);
 			$l[1] = new Lutador('Putscript', 'Brasil', 21, 1.68, 57.8, 14, 2, 3);
 			$l[2] = new Lutador('SnapShadow', 'EUA', 35, 1.65, 80.9, 12, 2, 1);
